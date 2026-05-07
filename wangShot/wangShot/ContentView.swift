6563e2d5beaab4fd8e7eb761199bc0250c290328
Wang Xiaodi <xiaodi.dennis.wang@gmail.com>
2026-05-08 06:32:59 +0800

    Create native macOS app shell

---
+++ wangShot/wangShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wangShot
+//
+//  Created by Dennis Hsiao Ti WANG on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wangShot/wangShot/wangShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  wangShotApp.swift
+//  wangShot
+//
+//  Created by Dennis Hsiao Ti WANG on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct wangShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
